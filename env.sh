e888547adbc2d99d84aa8eedb20f03ff34a9d0d9
Rock1ey <2061218098@qq.com>
2026-05-12 23:58:17 +0800

    多卡训练更改

---
+++ env.sh
@@ -0,0 +1,13 @@
+# 克隆项目
+git clone 
+
+# 在项目根目录下执行
+python -m pip install -U pip
+pip install -r requirements.txt
+pip install -e .
+
+# 零样本测试 BLIP-2 FLAN-T5-XL
+bash run_scripts/blip2/eval/eval_pathvqa_zeroshot_flant5xl.sh
+
+# 微调后测试：
+bash run_scripts/blip2/train/train_pathvqa_flant5xl.sh
